1f35414ac812fe12ff232b793f4d80691a59e693
techswarn <tech.swarns@gmail.com>
2025-05-27 15:46:43 +0530

    added sleep

--- public/index.php
+++ public/index.php
@@ -48,7 +48,7 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 | and wonderful application we have prepared for them.
 |
 */
-
+sleep(60);
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
